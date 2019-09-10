ae791b8b990cfb0f243ac3bdb1f5b176bd2b8968
Sergio Segaty <sergio.segaty@gmail.com>
2019-09-10 14:01:14 -0300

    Criado Seed no Sistema

---
+++ Repository/SeederSistemContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public static class SeederSistemContext
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(
+
+                new Categoria()
+                {
+                    Id = 1,
+                    Nome = "Eletrônicos",
+                    DataCriacao = DateTime.Now,
+                    RegistroAtivo = true
+                },
+                new Categoria()
+                {
+                    Id = 2,
+                    Nome = "Jogos de Tabuleiro",
+                    DataCriacao = DateTime.Now,
+                    RegistroAtivo = true
+                }
+
+
+
+
+            );
+
+        }
+
+    }
+}
