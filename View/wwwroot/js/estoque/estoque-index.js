c0535d1feb1be8851aa9d0bb2678f4bcedd998c5
Sergio Segaty <sergio.segaty@gmail.com>
2019-09-18 14:46:48 -0300

    Desenvolvimento Estoque-FooTable

--- View/wwwroot/js/estoque/estoque-index.js
+++ View/wwwroot/js/estoque/estoque-index.js
@@ -1,3 +1,20 @@
 ﻿jQuery(function ($) {
-    $('.table').footable();
+    $('#tabela-estoque').footable({
+        "columns": [
+            { "name": "id", "title": "Código", "breakpoints": "xs sm", "type": "number", "style": { "width": 80, "maxWidth": 80 } },
+            { "name": "nome", "title": "Nome" }
+        ],
+        "rows": $.get('/estado/obtertodos')
+    });
+
+    $('[name=status]').on('change', function () {
+        var filtering = FooTable.get('#tabela-estoque').use(FooTable.Filtering);
+        var filter = $(this).val(); 
+        if (filter === 'none') { // if the value is "none" remove the filter
+            filtering.removeFilter('status');
+        } else { // otherwise add/update the filter.
+            filtering.addFilter('status', filter, ['status']);
+        }
+        filtering.filter();
+    });
 });
