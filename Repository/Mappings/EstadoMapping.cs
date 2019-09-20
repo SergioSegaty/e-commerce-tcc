ef2e3428b334fc363cd02999f9808147f08826a6
Pedro Amorim <pedrohamr@gmail.com>
2019-09-20 17:22:22 -0300

    Criado EstadoMapping

--- Repository/Mappings/EstadoMapping.cs
+++ Repository/Mappings/EstadoMapping.cs
@@ -1,23 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Model.Entities;
 using System;
 
 namespace PadawanStore.Infra.Data.Mappings
 {
-    public class EstadoMapping : IEntityTypeConfiguration<Estado>
+    public class EstadoMapping : BaseConfiguration<Estado>
     {
-        public void Configure(EntityTypeBuilder<Estado> builder)
+        public override void Configure(EntityTypeBuilder<Estado> builder)
         {
+            base.Configure(builder);
+
             builder.ToTable("estados");
 
             builder.Property(e => e.Nome)
+                .HasColumnName("nome")
                 .HasColumnType("varchar(19)")
                 .HasMaxLength(19)
                 .IsRequired();
 
             builder.Property(e => e.Sigla)
+                .HasColumnName("sigla")
                 .HasColumnType("varchar(2)")
                 .HasMaxLength(2)
                 .IsRequired();
