0a6aac151b40b18f92e3644f15eacb7b37241cab
Pedro Amorim <pedrohamr@gmail.com>
2019-09-20 17:19:06 -0300

    Categoria - Removido DataAnnotations

--- Model/Entities/Categoria.cs
+++ Model/Entities/Categoria.cs
@@ -1,12 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Model.Entities
 {
-    [Table("categorias")]
     public class Categoria : Base
     {
-        [Column("nome"), StringLength(100, ErrorMessage = "No maximo 100 caracteres", MinimumLength = 3)]
         public string Nome { get; set; }
     }
 }
