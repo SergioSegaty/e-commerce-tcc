1e2b50beb65a9ba554e0da03998be2d7f0853c8c
Pedro Amorim <pedrohamr@gmail.com>
2019-09-20 17:19:42 -0300

    Marca - Removido DataAnnotations

--- Model/Entities/Marca.cs
+++ Model/Entities/Marca.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Model.Entities
+﻿namespace Model.Entities
 {
-    [Table("marcas")]
     public class Marca : Base
     {
-        [Column("nome")]
         public string Nome { get; set; }
     }
 }
