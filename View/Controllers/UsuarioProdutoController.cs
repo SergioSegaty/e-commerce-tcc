beebd155eda052a5d67c98ab924a725ca0920d65
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-09-22 16:03:02 -0300

    Prototipo de tela de produtos para usuario

---
+++ View/Controllers/UsuarioProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities;
+using Repository.Interface;
+
+namespace PadawanStore.Web.UI.Controllers
+{
+    [Route("usuarioproduto")]
+    public class UsuarioProdutoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IBaseRepositoryAsync<Produto> _repository;
+
+        public UsuarioProdutoController(IBaseRepositoryAsync<Produto> repository, IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+            _repository = repository;
+        }
+
+        [HttpGet, Route("")]    
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("obtertodos")]
+        public JsonResult ObterTodos()
+        {
+            var produtos = _produtoRepository.ObterTodos();
+            return Json(produtos);
+        }
+    }
+}
