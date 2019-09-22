70039d62523ea20d826c0bcb90824ed4724cb2a8
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-09-22 18:12:03 -0300

    Desenvolvimento ViewUsuario-GerenciamentoPerfil

---
+++ View/Controllers/ViewUsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities;
+using Repository.Interface;
+
+namespace PadawanStore.Web.UI.Controllers
+{
+    [Route("viewusuario")]
+    public class ViewUsuarioController : Controller
+    {
+        private readonly IBaseRepositoryAsync<Usuario> _repository;
+
+        public ViewUsuarioController(IBaseRepositoryAsync<Usuario> rep)
+        {
+            _repository = rep;
+        }
+
+        [HttpGet, Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
