4932fca496084734d9bdeef032b315c26ae70916
SergioSegaty <49919279+SergioSegaty@users.noreply.github.com>
2019-10-07 21:47:48 -0300

    Desenvolvimento da Pagina Produto por Id
    Agora tem Rating e Estrelas

--- src/LivrariaNerd.Web.UI/wwwroot/js/usuario/pegar-produto.js
+++ src/LivrariaNerd.Web.UI/wwwroot/js/usuario/pegar-produto.js
@@ -1,38 +1,6 @@
 ﻿$(() => {
-    $id = -1;
-    // Pegando os elementos do DOM para alterar
 
-    $categoriaTituloBC = document.getElementById('bc-titulo-produto');
-    $imagemProduto = document.getElementById('imagem-produto');
-    $nomeProduto = document.getElementById('nome-produto');
-    $precoProduto = document.getElementById('preco-produto');
-    $categoriaProduto = document.getElementById('categoria-produto');
-    $tituloProduto = document.getElementById('titulo-produto');
-
-
-    obterProduto = function () {
-
-        $.ajax({
-            url: 'produto/obterpeloid?=' + $id,
-            method: 'get',
-            success: function (data){
-                if (data != null) {
-
-                    $categoriaTituloBC.innerHtml = data.NomeCategoria;
-                    $imagemProduto = "/" + data.imagemCaminhoWwwroot;
-                    $nomeProduto.innerHtml = data.nome;
-                    $precoProduto.innerHtml = data.preco;
-                    $categoriaProduto.innerHtml = data.NomeCategoria;
-                    $tituloProduto.innerHtml = data.nome;
-                    
-                } else {
-                    notiftyAlert(3, 'Produto Não encontrado', 2);
-                }
-            },
-
-        });
-
-    };
+   // $('#rating-example').raty();
 
 
 });
